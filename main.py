396b93fd04d2a4f6e4227d07df5a95615ce8204a
pkpraveenkumar282 <pkpraveenkumar282@gmail.com>
2026-03-27 00:05:31 +0530

    Create main.py as the main entry point for the platform

---
+++ main.py
@@ -0,0 +1,5 @@
+# Main Entry Point for Nmap VirusTotal Analysis
+
+if __name__ == '__main__':
+    print("This is the main entry point for the Nmap VirusTotal analysis platform.")
+    # Add your implementation here.
